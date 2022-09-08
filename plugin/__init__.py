226cc166f583ea3c931fe955aa649f5ce1e78838
Andrew Lamoureux <andrew@vector35.com>
2022-09-08 11:28:06 -0400

    Don't output when running headless, issue #6

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,16 +1,20 @@
-import os
-import sys
-import inspect
+import binaryninja
 
-this_file = inspect.stack()[0][1]
-this_dir = os.path.dirname(this_file)
-this_dir = os.path.realpath(this_dir)
-cl_dir = os.path.join(this_dir, '..', 'corelogic')
-sys.path.append(cl_dir)
+if binaryninja.core_ui_enabled():
+    # MODE: GUI
 
-try:
+    # get the corelogic in syspath so plugin will be able to import kshelpers, kaitaistruct modules
+    import os
+    import sys
+    import inspect
+    this_file = inspect.stack()[0][1]
+    this_dir = os.path.dirname(this_file)
+    this_dir = os.path.realpath(this_dir)
+    cl_dir = os.path.join(this_dir, '..', 'corelogic')
+    sys.path.append(cl_dir)
+
+    # import the UI
     from . import view
-
-except Exception as e:
-    print(e)
-    print('Unable to import view, assuming commandline invocation.')
+else:
+    # MODE: headless
+    pass
